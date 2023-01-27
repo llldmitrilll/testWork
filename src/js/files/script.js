ee46d3fc303e5daae5ed254c22c4f4d8d9298138
llldmitrilll <drdmitri@yandex.ru>
2023-01-27 11:40:04 +0300

    menu burger

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,13 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+const mblockShowMore = document.querySelector('.mblock__button');
+const cardHiddens = document.querySelectorAll('.mblock__card--hidden');
+
+mblockShowMore.addEventListener('click', function () {
+   cardHiddens.forEach(function (card) {
+      card.classList.toggle('mblock__card--hidden');
+   })
+});
